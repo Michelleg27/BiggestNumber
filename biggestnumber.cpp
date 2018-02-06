227ce458203c4f46f97188e27088904524191c10
michelleg27 <mgomez48@mail.csuchico.edu>
2018-02-06 11:37:55 -0800

    Updated Version

--- biggestnumber.cpp
+++ biggestnumber.cpp
@@ -7,12 +7,21 @@ int main()
 {
 int wholeNum1;
 int wholeNum2;
+int biggest; 
 
   cout<<"Please enter a whole number:\n";
   cin>>wholeNum1;   
   cout<<"Please enter another whole number:\n";
   cin>>wholeNum2;
-  cout<<"Of those two numbers, the biggest is: ";
+  if ( wholeNum1 > wholeNum2)
+  { 
+    biggest = wholeNum1; 
+  }
+  else 
+  {
+    biggest = wholeNum2;
+  }
+  cout<<"Of those two numbers, the biggest is: "<<biggest<<endl;
   cout<<endl<<"Thank you for playing.\n";
 
   return 0;
